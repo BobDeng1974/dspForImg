127a72f0ccb9ce8002e841655f6db2e5fbc4bb5a
meiqua <563714976@qq.com>
2017-11-14 00:02:27 +0800

    image levels

--- imgprocesser.cpp
+++ imgprocesser.cpp
@@ -18,9 +18,13 @@ cv::Mat imgProcesser::process(cv::Mat &src)
     dst = src.clone();
 
     int levels = log2(dst.cols/16);
-    for(int i=0; i<levels ; i++){
-            pyrDown(dst,dst);
+    vector<cv::Mat> images(levels+1);
+    images[levels] = dst.clone();
+    for(int i=levels-1;i>=0;i--){
+        pyrDown(images[i+1],images[i]);
     }
+
+    dst = images[2];
 
 
 //    t_all = ((double)getTickCount() - t_all)*1000/getTickFrequency();
