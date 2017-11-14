b3cbf68de73450cda032699e1c192913ec095a13
meiqua <563714976@qq.com>
2017-11-14 15:52:02 +0800

    nothing new

--- imgprocesser.cpp
+++ imgprocesser.cpp
@@ -15,12 +15,6 @@ cv::Mat imgProcesser::process(cv::Mat &src)
     double t_all = (double)getTickCount();
 
     cv::Mat dst;
-    dst = src.clone();
-
-    int levels = log2(dst.cols/16);
-    for(int i=0; i<levels ; i++){
-            pyrDown(dst,dst);
-    }
 
 
 //    t_all = ((double)getTickCount() - t_all)*1000/getTickFrequency();
